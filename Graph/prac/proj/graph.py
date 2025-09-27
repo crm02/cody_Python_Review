9d86f16630b00c76148e0aade3156ad6a16c2a9a
Cody <codyrmill@gmail.com>
2025-09-26 21:31:01 -0400

    using FuncAnimation to animate function

--- Graph/prac/proj/graph.py
+++ Graph/prac/proj/graph.py
@@ -1,8 +1,56 @@
 import matplotlib.pyplot as plt
-from systemInfo import get_cpu_per
+from systemInfo import get_cpu_per, get_ram
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+def update(frame):
+    new_cpu =get_cpu_per() 
+    cpu.append(new_cpu)
+    line.set_data(range(len(cpu)),cpu)
+
+    new_ram = get_ram()
+    ram.append(new_ram)
+    line2.set_data(range(len(ram)),ram)
+
+    ax.relim()
+    ax.autoscale_view()
+    plt.annotate(f'{get_ram()}',xy=(get_ram(),get_ram()))
+
+    
 
 
-cpu_percentages = [get_cpu_per() for i in range(10)]
-plt.plot(cpu_percentages,'b-')
+cpu = []
+ram = []
+
+fig, ax = plt.subplots()
+line, = ax.plot([],[], label='CPU')
+line2, = ax.plot([],[],  label = 'RAM')
+ax.legend()
+
+ax.set_xlim(0,100)
+ax.set_ylim(0,100)
+ax.set_title('CPU and RAM Percentages')
+ax.set_xlabel('Time')
+ax.set_ylabel('%')
+
+
+
+
+animation = FuncAnimation(fig, update, frames=None, interval= 0.1)
 plt.show()
 
+
+#cpu_percentages = [get_cpu_per() for i in range(10)]
+#plt.plot(cpu_percentages,'b-')
+#
+#
+#
+#ram_per = [get_ram() for i in range(10)]
+#plt.plot(ram_per,'r-')
+#
+#
+#
+## Labels
+#plt.xlabel('Blue line CPU | Red line RAM',)
+#
+#plt.show()
--- Graph/prac/proj/systemInfo.py
+++ Graph/prac/proj/systemInfo.py
@@ -4,3 +4,7 @@ def get_cpu_per():
         cpu_perc = psutil.cpu_percent(interval =0.1) # since this isn't repeating bc its a func call, does inter
         # val matter here? The interval here affects our x axis! very cool
         return cpu_perc # returns cpu
+
+def get_ram():
+        ram = psutil.virtual_memory()
+        return ram[2]
